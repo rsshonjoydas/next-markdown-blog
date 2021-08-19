1b3fe31661a067e316c918d2c1655560899fa51d
rsshonjoydas <rsshonjoydas@gmail.com>
2021-08-19 20:35:03 +0600

    show post in ui

--- components/Post.js
+++ components/Post.js
@@ -1,10 +1,21 @@
+import Link from 'next/link';
 
-const Post = ({post}) => {
+const Post = ({ post }) => {
   return (
     <div className="card">
-      <img src={post.frontmatter.cover_image}/>
-    </div>
-  )
-}
+      <img src={post.frontmatter.cover_image} />
 
-export default Post
+      <div className="post-date">Post on {post.frontmatter.date}</div>
+
+      <h3>{post.frontmatter.title}</h3>
+
+      <p>{post.frontmatter.excerpt}</p>
+
+      <Link href={`/blog/${post.slug}`}>
+        <a className="btn">Read More</a>
+      </Link>
+    </div>
+  );
+};
+
+export default Post;
