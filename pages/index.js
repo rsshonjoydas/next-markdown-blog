26d0ef35a190389ab6ad18ab77d65c57c0c1e0ed
rsshonjoydas <rsshonjoydas@gmail.com>
2021-08-19 20:46:40 +0600

    post sort

--- pages/index.js
+++ pages/index.js
@@ -1,51 +1,52 @@
-import fs from 'fs';
-import matter from 'gray-matter';
-import Head from 'next/head';
-import path from 'path';
-import Post from '../components/Post';
+import fs from 'fs'
+import path from 'path'
+import matter from 'gray-matter'
+import Head from 'next/head'
+import Post from '../components/Post'
+import { sortByDate } from '../utils'
 
 export default function Home({ posts }) {
   return (
     <div>
       <Head>
-        <title>rsshonjoy | Blog</title>
+        <title>Dev Blog</title>
       </Head>
 
-      <div className="posts">
+      <div className='posts'>
         {posts.map((post, index) => (
-          <Post post={post} />
+          <Post key={index} post={post} />
         ))}
       </div>
     </div>
-  );
+  )
 }
 
 export async function getStaticProps() {
-  // ? Get files from the posts dir
-  const files = fs.readdirSync(path.join('posts'));
+  // Get files from the posts dir
+  const files = fs.readdirSync(path.join('posts'))
 
-  // ? Get slug and frontmatter from posts
+  // Get slug and frontmatter from posts
   const posts = files.map((filename) => {
-    // ? Create slug
-    const slug = filename.replace('.md', '');
+    // Create slug
+    const slug = filename.replace('.md', '')
 
-    // ? Get frontmatter
+    // Get frontmatter
     const markdownWithMeta = fs.readFileSync(
       path.join('posts', filename),
       'utf-8'
-    );
+    )
 
-    const { data: frontmatter } = matter(markdownWithMeta);
+    const { data: frontmatter } = matter(markdownWithMeta)
 
     return {
       slug,
       frontmatter,
-    };
-  });
+    }
+  })
 
   return {
     props: {
-      posts,
+      posts: posts.sort(sortByDate),
     },
-  };
+  }
 }
