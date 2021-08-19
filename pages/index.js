d03773b9e79fe82486b7bc4ac6534600f424bed5
rsshonjoydas <rsshonjoydas@gmail.com>
2021-08-19 20:08:17 +0600

    markdown show ui

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,50 @@
+import fs from 'fs';
+import matter from 'gray-matter';
 import Head from 'next/head';
+import path from 'path';
 
-export default function Home() {
+export default function Home({ posts }) {
   return (
     <div>
       <Head>
         <title>rsshonjoy | Blog</title>
       </Head>
 
-      <h2>Hello</h2>
+      <div className="posts">
+        {posts.map((post, index) => (
+          <h3>{post.frontmatter.title}</h3>
+        ))}
+      </div>
     </div>
   );
+}
+
+export async function getStaticProps() {
+  // ? Get files from the posts dir
+  const files = fs.readdirSync(path.join('posts'));
+
+  // ? Get slug and frontmatter from posts
+  const posts = files.map((filename) => {
+    // ? Create slug
+    const slug = filename.replace('.md', '');
+
+    // ? Get frontmatter
+    const markdownWithMeta = fs.readFileSync(
+      path.join('posts', filename),
+      'utf-8'
+    );
+
+    const { data: frontmatter } = matter(markdownWithMeta);
+
+    return {
+      slug,
+      frontmatter,
+    };
+  });
+
+  return {
+    props: {
+      posts,
+    },
+  };
 }
---
+++ posts/test.md
@@ -0,0 +1,9 @@
+---
+title: 'Test Post'
+date: '19/08/2021'
+excerpt: 'This is the excerpt'
+cover_image: '/images/posts/img1.jpg'
+---
+## Heading Two
+
+* Item 1
